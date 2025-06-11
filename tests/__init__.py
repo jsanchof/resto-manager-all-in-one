600725593ecf7c9ce8fa289ef5d22b8b3dd927b6
jsanchof <jose.sancho89@gmail.com>
2025-06-10 18:11:58 -0600

    removing test, will focus on correcting the CI, then will add test with Jest

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Test package initialization.
-Import all test modules here to ensure they are discovered by pytest.
-"""
-from . import test_auth
-from . import test_database
-from . import test_orders
